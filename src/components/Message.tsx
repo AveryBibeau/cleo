0edf64774bb12cd155f7ac0bbb9467335d2978fa
Avery Bibeau <averybibeau@gmail.com>
2022-09-17 15:35:53 -0700

    initial cleanup

--- src/components/Message.tsx
+++
@@ -1,5 +0,0 @@
-import { h, FunctionComponent } from 'preact'
-
-export const MessageComponent: FunctionComponent<{ message?: string }> = ({ message }) => (
-  <p class="font-bold">Lorem asdf: {message}</p>
-)
